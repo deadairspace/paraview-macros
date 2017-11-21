81a055114a46ac4f4c1122dfa6d0486d7d61e260
Travleev <anton.travleev@kit.edu>
2017-11-21 13:12:56 +0100

    Implemented storing screenshot figures using names of view windows.

--- macros/screenshots.py
+++ macros/screenshots.py
@@ -12,7 +12,8 @@ os.mkdir(basefolder)
 
 # Save the state, used for screenshots:
 SaveState(os.path.join(basefolder, 'state.pvsm'))
-# Each layout is stored in separate folder
+# Each layout is stored in separate folder, figure names -- from the view titles
+vd = servermanager.ProxyManager().GetProxiesInGroup('views')
 print 'Screenshots:'
 for (name, i), l in GetLayouts().items():
     folder = os.path.join(basefolder, name)
@@ -22,7 +23,11 @@ for (name, i), l in GetLayouts().items():
         # are not used for 2 reasons: (1) I don't know how to get this name and
         # (2) these names are not unique (there can be two views with the same
         # window name. 
-        fname = os.path.join(folder, '{}.png'.format(iv))
+        for (n, i), vv in vd.items():
+            if vv is v:
+                name = n
+                vd.pop((n, i))
+        fname = os.path.join(folder, '{}.png'.format(name))
         print fname, 
         if SaveScreenshot(fname, v):
             print 'ok'
