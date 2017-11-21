6c3bb2173f5999feffc3b50749cc725e9f7f9a2b
travleev <anton.travleev@kit.edu>
2017-11-21 02:23:28 +0100

    Added description

--- macros/2d_light.py
+++ macros/2d_light.py
@@ -1,3 +1,11 @@
+# By default, the colors in a paraview render view are modified to present 3d objects.
+# This helps to understand the 3d shape of the objects, but makes the colors differ 
+# from the color scale. For 2D maps, where three-dimensional form of the object is
+# not important, another set of lights is more suitable.
+#
+# This script changes for all render views with the interaction mode set to "2D" the
+# set of lights, so that the colors on the map exactly the same as colors on the scale.
+
 for v in GetRenderViews():
     if v.InteractionMode == '2D':
         v.UseLight = False
